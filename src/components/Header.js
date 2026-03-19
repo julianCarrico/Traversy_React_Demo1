5af8f92008fb797c0254a31d319de8fa3e47e49a
Julian Carrico <Julian.carrico@gmail.com>
2026-03-19 15:07:16 -0400

    part 1 of completeding Traversy React course with updated changes from 2019

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button';
+
+const Header = ({ title = 'Task Tracker' }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
+    return (
+        <header className='header'>
+            {/* Now it uses the passed 'title' or the default 'Task Tracker' */}
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
+        </header>
+    );
+};
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = { 
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+
+export default Header
