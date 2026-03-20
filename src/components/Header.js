8a8e5823071e3f24ae2a7106994038425e29f047
Julian Carrico <Julian.carrico@gmail.com>
2026-03-20 16:04:09 -0400

    completed tutorial with json server as backend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types'
 import Button from './Button';
 
-const Header = ({ title = 'Task Tracker' }) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
+const Header = ({ title = 'Task Tracker', onAdd, showAdd }) => {
     return (
         <header className='header'>
             {/* Now it uses the passed 'title' or the default 'Task Tracker' */}
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
         </header>
     );
 };
